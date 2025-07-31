8de2830f90f815bdbeac8d11e53411d87d8089ac
Gabriel O. <gabriele.kkj@gmail.com>
2025-07-31 08:34:54 +0200

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Link} from "react-router";
+
+function App() {
+
+  return (
+    <nav>
+        <Link to={"/"} style={{paddingRight: "10px"}}> test </Link>
+
+    </nav>
+  )
+}
+
+export default App
